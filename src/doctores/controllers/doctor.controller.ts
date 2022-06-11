91a4f9a878aec43020dfb93bf7fdc7eec3438acf
carlosead1410 <arriaga1410@gmail.com>
2022-06-11 15:14:59 -0400

    Megre rama prueba

--- src/doctores/controllers/doctor.controller.ts
+++ src/doctores/controllers/doctor.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-<<<<<<< HEAD
-=======
 import { BaseController } from 'src/commons/controller.commons';
 import { BaseService } from 'src/commons/service.commonts';
 import { Doctores } from '../entities/doctor.entity';
->>>>>>> 3d57650529cc79995ca5bfea3902674fad7cb953
 import { DoctorService } from '../services/doctor.service';
 
 @Controller('api/doctor')
@@ -17,15 +14,9 @@ export class DoctorController {
         return this.doctorServices.getAllDoctor();
     }
 
-<<<<<<< HEAD
-    @Get('prueba/:especialidadName')
-    Prueba(@Param('especialidadName') especialidadName: string ){
-        return this.doctorServices.findByEspeciality(especialidadName);
-=======
     @Get('especialidad/:nombre')
     buscarEspecialidad(@Param('nombre') nombre){
         return this.doctorServices.getEspecialidad(nombre)
->>>>>>> 3d57650529cc79995ca5bfea3902674fad7cb953
     }
 
    
