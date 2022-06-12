4c72c60d4ae299eefb4c7d008e9ba6c675b8bb99
carlosead1410 <arriaga1410@gmail.com>
2022-06-12 18:01:15 -0400

    Quitando el async y await

--- src/doctores/services/doctor.service.ts
+++ src/doctores/services/doctor.service.ts
@@ -6,18 +6,18 @@ export class DoctorService {
     
     constructor(private readonly docRepo: DoctorRepository){}
 
-    async getAllDoctor(){
-        return await this.docRepo.findAll();
+    getAllDoctor(){
+        return this.docRepo.findAll();
     }
 
-    async getEspecialidad(especialidad: string){
+    getEspecialidad(especialidad: string){
         //Si envio una especialidad, buscamos por esa especialidad
         if(especialidad){
             especialidad = especialidad.toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-            return await this.docRepo.findByEspeciality(especialidad);
+            return this.docRepo.findByEspeciality(especialidad);
         }else{
             //caso en el que no indico especialidad
-            return await this.getAllDoctor();
+            return this.getAllDoctor();
         }
     }
 
