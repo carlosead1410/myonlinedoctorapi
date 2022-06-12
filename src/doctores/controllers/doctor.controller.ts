29eef586f86bc9de0a0d5dc24e3cc7a8aed31c10
carlosead1410 <arriaga1410@gmail.com>
2022-06-12 16:18:14 -0400

    Colocando metodos asincronos

--- src/doctores/controllers/doctor.controller.ts
+++ src/doctores/controllers/doctor.controller.ts
@@ -8,14 +8,15 @@ export class DoctorController {
     constructor(private readonly doctorServices: DoctorService){}
 
     @Get('all')
-    buscarDoctores(){
-        return this.doctorServices.getAllDoctor();
+    async buscarDoctores(){
+
+        return  await this.doctorServices.getAllDoctor();
     }
 
     @Get('filter/especiality')
     @HttpCode(200)
-    buscarEspecialidad(@Query('especialidad') especialidad:string){
-        return this.doctorServices.getEspecialidad(especialidad)
+    async buscarEspecialidad(@Query('especialidad') especialidad:string){
+        return await this.doctorServices.getEspecialidad(especialidad)
     }
 
    
--- src/doctores/services/doctor.service.ts
+++ src/doctores/services/doctor.service.ts
@@ -10,18 +10,18 @@ export class DoctorService {
     
     constructor(private readonly docRepo: DoctorRepository){}
 
-    getAllDoctor(){
-        return this.docRepo.findAll();
+    async getAllDoctor(){
+        return await this.docRepo.findAll();
     }
 
-    getEspecialidad(especialidad: string){
+    async getEspecialidad(especialidad: string){
         //Si envio una especialidad, buscamos por esa especialidad
         if(especialidad){
             especialidad = especialidad.toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-            return this.docRepo.findByEspeciality(especialidad);
+            return await this.docRepo.findByEspeciality(especialidad);
         }else{
             //caso en el que no indico especialidad
-            return this.getAllDoctor();
+            return await this.getAllDoctor();
         }
     }
 
