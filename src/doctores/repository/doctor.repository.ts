64ecb479701c3e9d802e3bf50e94d805a8b85ff6
carlosead1410 <arriaga1410@gmail.com>
2022-06-19 18:21:37 -0400

    Try Catch

--- src/doctores/repository/doctor.repository.ts
+++ src/doctores/repository/doctor.repository.ts
@@ -7,24 +7,35 @@ import { Doctores } from "../entities/doctor.entity";
 export class DoctorRepository extends Repository<Doctores>{
 
     async findAll(): Promise<Doctores[]>{
-        let list_doctores = await this.find({
-            relations: ["especialidades"],
-        });
-        return list_doctores;
+        try{
+            let list_doctores = await this.find({
+                relations: ["especialidades"],
+            });
+            return list_doctores;
+        }
+        catch(err){
+            console.log(err);
+        }
+        
     }
 
 
 
     async findByEspeciality(especialidad: string): Promise<Doctores[]>{
+        try{
+            const doctoresFiltrados =  await this.createQueryBuilder('doctores')
+            .leftJoin('doctores.especialidades', 'especialidades')
+            .leftJoinAndSelect('doctores.especialidades', 'EspecialidadesSelect')
+            .where('especialidades.nombre like :especialidad', { especialidad: `%${especialidad}%`})
+            .orderBy('doctores.id_doctor', 'ASC')
+            .getMany();
 
-        const doctoresFiltrados =  await this.createQueryBuilder('doctores')
-                            .leftJoin('doctores.especialidades', 'especialidades')
-                            .leftJoinAndSelect('doctores.especialidades', 'EspecialidadesSelect')
-                            .where('especialidades.nombre like :especialidad', { especialidad: `%${especialidad}%`})
-                            .orderBy('doctores.id_doctor', 'ASC')
-                            .getMany();
-
-        return doctoresFiltrados;
+            return doctoresFiltrados;
+        }
+        
+        catch(err){
+            console.log(err);
+        }
 
     }
 
